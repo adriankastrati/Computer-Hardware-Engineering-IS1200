3427a070563476439e967507f378ee0fa3932f66
Adrian Kastrati <77054963+adriankastrati@users.noreply.github.com>
2022-02-15 21:02:03 +0100

    assignment 1, done

--- lab-3/time4io/mipslabwork.c
+++ lab-3/time4io/mipslabwork.c
@@ -31,21 +31,49 @@ void labinit( void ){
 
   volatile int *p_PORTE = 0xbf886110;
   *p_PORTE &= ~0xff;
+
+  TRISD &= 0xfe0;
 }
 
 /* This function is called repetitively from the main program */
 void labwork( void ){
+  int button_value = getbtns() & 0x3;
+  int btn_2 = getbtns() & 0x1;
+  int btn_3 = (getbtns() >> 1) & 0x1;
+  int btn_4 = (getbtns() >> 2) & 0x1;
+
+  int switch_value = getsw() & 0xf;
+
+
+  if(btn_2){
+    int new_time = (mytime & 0x0fff) | (switch_value << 12);
+    mytime = new_time; 
+  }
+  
+  if(btn_3){
+    int new_time = (mytime & 0xf0ff) | (switch_value << 8);
+    mytime = new_time;  
+  }
+
+  if(btn_4){
+    int new_time = (mytime & 0xff0f) | (switch_value << 4);
+    mytime = new_time;  
+  }
+
 
   delay( 1000 );
   time2string( textstring, mytime );
   display_string( 3, textstring );
   display_update();
+
+
   tick( &mytime );
  
   volatile int *p_PORTE = 0xbf886110;
 
   *p_PORTE = (*p_PORTE & ~0xff) + (((char) *p_PORTE & 0xff) + 1);
   
+ 
 
   display_image(96, icon);
 }
