36906bca4b3b11e3a457acdc181361a11cdfc603
Adrian Kastrati <77054963+adriankastrati@users.noreply.github.com>
2022-02-16 14:46:24 +0100

    update

--- lab-3/time4timer/mipslabwork.c
+++ lab-3/time4timer/mipslabwork.c
@@ -26,46 +26,26 @@ void user_isr( void ){
 
 /* Lab-specific initialization goes here */
 void labinit( void ){
-  //sets bits as output
   volatile int *p_TRISE =  0xbf886100;
   *p_TRISE &= ~0xff; 
 
-  
   volatile int *p_PORTE = 0xbf886110;
   *p_PORTE &= ~0xff;
 
-  // set bits of 1111 1110 000 to input (buttons)
-  TRISD |= 0xfe0;
-
-  //add code to initialize Timer 2 for timeouts every 100 ms (that is 10 timeouts per second). 
-  //Be prepared to justify your choices of clock-rate divider and time-out period.
-
-  //Timer 2 operates at 80 MHz
-/*
-  //clears timer 2 bit 15 and 1
-  T2CONCLEAR = 1 << 15;
-  T2CONCLEAR = 1 << 1;
-  
-  //resets the timervalue
-  TMR2 = 0x0;
-  
-  //prescale
-  T2CONSET = 101 << 4; 
-
-  //turn on timer
-  T2CONSET = 1 << 15;
-  */
-  }
+  TRISD &= 0xfe0;
+}
 
 /* This function is called repetitively from the main program */
 void labwork( void ){
+  int button_value = getbtns() & 0x3;
   int btn_2 = getbtns() & 0x1;
   int btn_3 = (getbtns() >> 1) & 0x1;
   int btn_4 = (getbtns() >> 2) & 0x1;
 
   int switch_value = getsw() & 0xf;
 
-  if(btn_4){
+
+  if(btn_2){
     int new_time = (mytime & 0x0fff) | (switch_value << 12);
     mytime = new_time; 
   }
@@ -75,21 +55,25 @@ void labwork( void ){
     mytime = new_time;  
   }
 
-  if(btn_2){
+  if(btn_4){
     int new_time = (mytime & 0xff0f) | (switch_value << 4);
     mytime = new_time;  
   }
 
-  volatile int *p_PORTE = 0xbf886110;
-  
-  //increments value by 1
-  *p_PORTE =  ((*p_PORTE & 0xff) + 1);
 
   delay( 1000 );
   time2string( textstring, mytime );
   display_string( 3, textstring );
   display_update();
+
+
   tick( &mytime );
+ 
+  volatile int *p_PORTE = 0xbf886110;
+
+  *p_PORTE = (*p_PORTE & ~0xff) + (((char) *p_PORTE & 0xff) + 1);
+  
+ 
 
   display_image(96, icon);
 }
